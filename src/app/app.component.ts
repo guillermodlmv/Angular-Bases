2227d607b96e9e67fd84b0dba206959492533390
Guillermo Villagomez <gvdelamora@gmail.com>
2022-01-11 03:16:23 -0600

    Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title : string = 'Contador App';
-  numero: number = 10;
-  base  : number = 5;
 
-  acumular( valor:number ){
-    this.numero += valor;
-  }
 }
